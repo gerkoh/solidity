ccffa479776e91da8e23fd2ccd71baf778e00b50
Gerald Koh Jia Le <geraldkoh123@gmail.com>
2023-05-16 20:14:38 +0800

    Base contract complete, working on tests

--- src/BasicVault.sol
+++ src/BasicVault.sol
@@ -1,29 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
-import ERC20Mock.sol;
+import {IERC20} from "../lib/forge-std/src/interfaces/IERC20.sol";
 
-contract BasicVault is ERC20Mock {
+contract BasicVault {
     
     // STATE VARS:
 
     // token
+    IERC20 public token;
 
     // mapping to serve as balances registry
-    mapping address => uint256 public balances;
+    mapping (address => uint256) public balances;
 
     // events 
     event Deposit(address indexed recipient, uint256 amount);
     event Withdrawal(address indexed sender, uint256 amount);
     
     // constructor function
-    constructor(string memory name, string memory symbol) ERC20Mock(name, symbol) public {
+    constructor(IERC20 token_) public {
+        token = token_;
     }
 
     // deposit function
-    function deposit(address recipient, uint256 amount) public {
-        balances[msg.sender] += amount;
-        bool success = wmdToken.transfer(msg.sender, amount);
+    // transferFrom is initiated by the recipient
+    // transfer is initiated by the sender
+    function deposit(uint256 amount) public {
+        // balances[address[this]] -= amount;
+        balances[msg.sender] += amount; // record keeping
+        bool success = token.transferFrom(msg.sender, address(this), amount); // actual transfer
         require(success, "Insufficient balance");
         emit Deposit(msg.sender, amount);
     }
@@ -32,7 +37,7 @@ contract BasicVault is ERC20Mock {
     // withdraw function
     function withdrawal(uint256 amount) public {
         balances[msg.sender] -= amount;
-        bool success = wmdToken.transfer(msg.sender, amount);
+        bool success = token.transfer(msg.sender, amount);
         require(success, "Insufficient balance");
         emit Withdrawal(msg.sender, amount);       
     }
--- test/BasicVault.t.sol
+++ test/BasicVault.t.sol
@@ -3,5 +3,29 @@ pragma solidity ^0.8.15;
 
 import "forge-std/Test.sol";
 import "forge-std/console2.sol";
-import "../src/Solidity-workshop.sol";
+import "../src/BasicVault.sol";
 
+// Testing guidelines
+// All state variable changes in the contracts that you code.
+// All state variable changes in other contracts caused by calls from contracts that you code.
+// All require or revert in the contracts that you code.
+// All events being emitted.
+// All return values in contracts that you code.
+
+abstract contract StateZero is Test {
+    BasicVault public basicVault;
+    address public user;
+
+    function setUp() public virtual {
+        // deploy contract
+        basicVault = new BasicVault(IERC20(address(1)));
+
+        // initialise variables
+        user = address(1);
+        vm.label(user, "user");
+    }
+}
+
+// deployment
+// deposit
+// withdraw
