b1f6783db43d123ef9a9d0bd9ef666338768f46a
Gerald Koh Jia Le <geraldkoh123@gmail.com>
2023-05-18 21:32:17 +0800

    Incomplete ETHWrapper test

---
+++ test/ETHWrapper.t.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import "forge-std/Test.sol";
+import "forge-std/console2.sol";
+import "../src/Solidity-workshop.sol";
+
+abstract contract StateZero is Test {
+    function setUp() {
+        
+    }
+}
+
+contract StateZeroTest is StateZero {
+
+}
