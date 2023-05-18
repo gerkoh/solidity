d415d6b539287369d2df67fa9d148c44635b3f28
Gerald Koh Jia Le <geraldkoh123@gmail.com>
2023-05-18 20:34:08 +0800

    Third commit

--- src/ETHWrapper.sol
+++ src/ETHWrapper.sol
@@ -1,17 +1,46 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
-import {ERC20} from "../lib/forge-std/src/interfaces/IERC20.sol";
+import {ERC20} from "../lib/yield-utils-v2/src/token/ERC20.sol";
 
 contract ETHWrapper is ERC20 {
-    IERC20 public wrapppedtoken;
+    ERC20 public token;
 
     mapping (address => uint256) public balances;
 
-    event Deposit(address indexed recipient, uint256 amount);
-    event Withdrawal(address indexed sender, uint256 amount);
+    event Wrapped(address indexed recipient, uint256 amount);
+    event Unwrapped(address indexed sender, uint256 amount);
 
-    constructor(IERC20 wrappedtoken_, IERC20 ) public {
-        wrapppedtoken = wrappedtoken_;
+    constructor(ERC20 token_, string memory name_, string memory symbol_, uint8 decimals_) ERC20(name_, symbol_, decimals_) {
+        token = token_;
+    }
+    
+    function mint(address dst, uint wad) internal {
+        _mint(dst, wad);
+    }
+
+    function burn(address src, uint wad) internal {
+        _burn(src, wad);
+    }
+
+    // Wrapping ERC20 to ERC20
+    function wrap(uint amount) public {
+        // uint256 amount = msg.value; // eth
+        mint(msg.sender, amount);
+        balances[msg.sender] += amount; // record keeping
+        bool success = token.transferFrom(msg.sender, address(this), amount); // actual transfer
+        require(success, "Unsuccessful wrapping");
+        emit Wrapped(msg.sender, amount);
+    }
+
+    function unwrapped(uint amount) public {
+        // uint256 amount = msg.value; // eth
+        require(this.balanceOf(msg.sender) >= amount, "Insufficient balance");
+        // require(balances[msg.sender] >= amount, "Insufficient balance")
+        burn(msg.sender,amount);
+        balances[msg.sender] -= amount; // record keeping
+        bool success = token.transferFrom(address(this), msg.sender, amount); // actual transfer
+        require(success, "Unsuccessful unwrapping");
+        emit Unwrapped(msg.sender, amount);
     }
 }
--- src/Token.sol
+++ src/Token.sol
@@ -1,45 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
-import {IERC20} from "../lib/forge-std/src/interfaces/IERC20.sol";
+import {ERC20} from "../lib/yield-utils-v2/src/token/ERC20.sol";
 
-contract Token {
-    
-    // STATE VARS:
-
+contract Token is ERC20 {
     // token
-    IERC20 public token;
+    ERC20 public token;
+    
 
     // mapping to serve as balances registry
     mapping (address => uint256) public balances;
-
-    // events 
-    event Deposit(address indexed recipient, uint256 amount);
-    event Withdrawal(address indexed sender, uint256 amount);
     
     // constructor function
-    constructor(IERC20 token_) public {
+    constructor(ERC20 token_, string memory name_, string memory symbol_, uint8 decimals_) ERC20(name_, symbol_, decimals_) {
         token = token_;
-    }
-
-    // deposit function
-    // transferFrom is initiated by the recipient
-    // transfer is initiated by the sender
-    function deposit(uint256 amount) public {
-        // balances[address[this]] -= amount;
-        balances[msg.sender] += amount; // record keeping
-        bool success = token.transferFrom(msg.sender, address(this), amount); // actual transfer
-        require(success, "Insufficient balance");
-        emit Deposit(msg.sender, amount);
-    }
-    
-
-    // withdraw function
-    function withdrawal(uint256 amount) public {
-        balances[msg.sender] -= amount;
-        bool success = token.transfer(msg.sender, amount);
-        require(success, "Insufficient balance");
-        emit Withdrawal(msg.sender, amount);       
-    }
-    
+    }   
 }
